f1f46fe0506284d1c99971c52a5179fd8ac3d171
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2020-11-16 19:53:18 +0000

    Remove application content.

--- applicationModules/example/routingProviders/api.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-const { Api } = require('@s1lv3rsph3r3/metropolitan');
-/**
- * Example usage is as follows:
- * A simple API GET request => Api.get('/admin', (req, res) => { res.send('HELLO WORLD'); });
- * A simple API POST request => Api.post('/admin', (req, res) => { res.send('HELLO WORLD'); });
- */
--- applicationModules/example/routingProviders/events.js
+++
@@ -1,6 +0,0 @@
-const { Event } = require('@s1lv3rsph3r3/metropolitan');
-
-// Event.subscribe('ExampleEvent').publish('ExampleEventController@exampleEvent');
-Event.subscribe('ExampleEvent').publish(() => {
-  /* Empty publish handler */
-});
